54ff9b7b5f02a4185958657358f00e4913a1d29c
Miguel Martinez <miguel.emezero@gmail.com>
2023-04-17 22:18:14 -0600

    👾 feat: hook to request data from SW api [breaking change]

---
+++ hooks/useStarships.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+const URL = 'https://swapi.dev/api/'
+
+export default function useStarships() {
+  const [data, setData] = useState()
+  const [hasMore, setHasMore] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`${URL}/starships?page=${page}`, {
+      method: 'GET'
+    }).then(res => res.json())
+      .then(res => {
+        setData([...(data || []), ...res.results])
+        setHasMore(res.next !== null)
+        setLoading(false)
+      })
+  }, [page])
+
+  return { data, loading, hasMore, setPage }
+}
