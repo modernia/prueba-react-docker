92c011593f2321e8ed5a7ee6047c6d3119a707bd
Miguel Martinez <miguel.emezero@gmail.com>
2023-04-21 16:08:51 -0600

    ⚡ perf: remove unused components

--- main.jsx
+++ main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './style.css'
-import App from './src/App'
 import AppTest from './src/AppTest'
 
 const root = createRoot(document.getElementById('app'))
